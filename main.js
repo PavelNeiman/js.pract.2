739eea354ed86b2425c6dd816dae092fba45f15a
Pavel <bonyown2012@gmail.com>
2019-01-18 23:05:22 +0300

    it's work!

---
+++ main.js
@@ -0,0 +1,21 @@
+var btn = document.querySelector('input.startBtn');
+var ph = document.querySelector('p');
+var val = document.querySelector('.bValue');
+
+
+
+
+
+function counter(b){
+    b--;
+    console.log('осталось ' + b + ' заходов');
+    if (b > 0){
+        counter(b);
+    }
+}
+
+
+btn.onclick = function(){
+    this.style.backgroundColor = 'red';
+    console.log('ya rabotay')
+}
